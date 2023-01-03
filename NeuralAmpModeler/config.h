fa75f12fc4a8900783c41efeca1923104a511084
Steven Atkinson <steven@atkinson.mn>
2023-01-02 18:49:20 -0800

    Version 0.6.0
    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- NeuralAmpModeler/config.h
+++ NeuralAmpModeler/config.h
@@ -1,7 +1,7 @@
 #define PLUG_NAME "NeuralAmpModeler"
 #define PLUG_MFR "Steven Atkinson"
-#define PLUG_VERSION_HEX 0x00000508
-#define PLUG_VERSION_STR "0.5.8"
+#define PLUG_VERSION_HEX 0x00000600
+#define PLUG_VERSION_STR "0.6.0"
 #define PLUG_UNIQUE_ID '1YEo'
 #define PLUG_MFR_ID 'SDAa'
 #define PLUG_URL_STR "https://github.com/sdatkinson/NeuralAmpModelerPlugin"
