ee9775864ba7fd6f984d4ea53b25111834c662b4
Steven Atkinson <steven@atkinson.mn>
2023-02-04 20:04:32 -0800

    Apply LLVM style, add a formatting helper script

--- NeuralAmpModeler/dsp/ImpulseResponse.h
+++ NeuralAmpModeler/dsp/ImpulseResponse.h
@@ -13,36 +13,37 @@
 
 #include <Eigen/Dense>
 
-#include "wdlstring.h"  // WDL_String
-#include "IPlugConstants.h"  // sample
+#include "IPlugConstants.h" // sample
 #include "dsp.h"
 #include "wav.h"
+#include "wdlstring.h" // WDL_String
 
 namespace dsp {
-  class ImpulseResponse : public History {
-  public:
-    ImpulseResponse(const WDL_String& fileName, const double sampleRate);
-    iplug::sample** Process(iplug::sample** inputs,
-                            const size_t numChannels,
-                            const size_t numFrames) override;
-    // TODO states for the IR class
-    dsp::wav::LoadReturnCode GetWavState() const { return this->mWavState; };
-  private:
-    // Set the weights, given that the plugin is running at the provided sample rate.
-    void _SetWeights(const double sampleRate);
-    
-    // State of audio
-    dsp::wav::LoadReturnCode mWavState;
-    // Keep a copy of the raw audio that was loaded so that it can be resampled
-    std::vector<float> mRawAudio;
-    double mRawAudioSampleRate;
-    // Resampled to the required sample rate.
-    std::vector<float> mResampled;
-    
-    const size_t mMaxLength = 8192;
-    // The weights
-    Eigen::VectorXf mWeight;
-  };
+class ImpulseResponse : public History {
+public:
+  ImpulseResponse(const WDL_String &fileName, const double sampleRate);
+  iplug::sample **Process(iplug::sample **inputs, const size_t numChannels,
+                          const size_t numFrames) override;
+  // TODO states for the IR class
+  dsp::wav::LoadReturnCode GetWavState() const { return this->mWavState; };
+
+private:
+  // Set the weights, given that the plugin is running at the provided sample
+  // rate.
+  void _SetWeights(const double sampleRate);
+
+  // State of audio
+  dsp::wav::LoadReturnCode mWavState;
+  // Keep a copy of the raw audio that was loaded so that it can be resampled
+  std::vector<float> mRawAudio;
+  double mRawAudioSampleRate;
+  // Resampled to the required sample rate.
+  std::vector<float> mResampled;
+
+  const size_t mMaxLength = 8192;
+  // The weights
+  Eigen::VectorXf mWeight;
 };
+}; // namespace dsp
 
 #endif /* ImpulseResponse_h */
--- NeuralAmpModeler/dsp/get_dsp.cpp
+++ NeuralAmpModeler/dsp/get_dsp.cpp
@@ -1,51 +1,48 @@
 #include <fstream>
 #include <unordered_set>
 
+#include "HardCodedModel.h"
 #include "dsp.h"
 #include "json.hpp"
 #include "lstm.h"
-#include "wavenet.h"
 #include "numpy_util.h"
-#include "HardCodedModel.h"
+#include "wavenet.h"
 
-
-void verify_config_version(const std::string version)
-{
-  const std::unordered_set<std::string> supported_versions({"0.2.0", "0.2.1", "0.3.0", "0.3.1", "0.4.0", "0.5.0"});
+void verify_config_version(const std::string version) {
+  const std::unordered_set<std::string> supported_versions(
+      {"0.2.0", "0.2.1", "0.3.0", "0.3.1", "0.4.0", "0.5.0"});
   if (supported_versions.find(version) == supported_versions.end())
     throw std::runtime_error("Unsupported config version");
 }
 
-std::vector<float> _get_weights(
-    nlohmann::json const& j,
-    const std::filesystem::path config_path)
-{
-    if (j.find("weights") != j.end()) { // New-style model
-        auto weight_list = j["weights"];
-        std::vector<float> weights;
-        for (auto it = weight_list.begin(); it != weight_list.end(); ++it)
-            weights.push_back(*it);
-        return weights;
+std::vector<float> _get_weights(nlohmann::json const &j,
+                                const std::filesystem::path config_path) {
+  if (j.find("weights") != j.end()) { // New-style model
+    auto weight_list = j["weights"];
+    std::vector<float> weights;
+    for (auto it = weight_list.begin(); it != weight_list.end(); ++it)
+      weights.push_back(*it);
+    return weights;
+  } else { // Old-style config.json + weights.npy
+    std::filesystem::path weights_path =
+        config_path.parent_path() / std::filesystem::path("weights.npy");
+    if (!std::filesystem::exists(weights_path)) {
+      std::stringstream s;
+      s << "No weights in model file, and could not find accompanying weights "
+           "at expected location "
+        << weights_path;
+      throw std::runtime_error(s.str());
     }
-    else {  // Old-style config.json + weights.npy
-        std::filesystem::path weights_path = config_path.parent_path() / std::filesystem::path("weights.npy");
-        if (!std::filesystem::exists(weights_path)) {
-            std::stringstream s;
-            s << "No weights in model file, and could not find accompanying weights at expected location " << weights_path;
-            throw std::runtime_error(s.str());
-        }
-        return numpy_util::load_to_vector(weights_path);
-    }
+    return numpy_util::load_to_vector(weights_path);
+  }
 }
 
-std::unique_ptr<DSP> get_dsp_legacy(const std::filesystem::path model_dir)
-{
+std::unique_ptr<DSP> get_dsp_legacy(const std::filesystem::path model_dir) {
   auto config_filename = model_dir / std::filesystem::path("config.json");
   return get_dsp(config_filename);
 }
 
-std::unique_ptr<DSP> get_dsp(const std::filesystem::path config_filename)
-{
+std::unique_ptr<DSP> get_dsp(const std::filesystem::path config_filename) {
   if (!std::filesystem::exists(config_filename))
     throw std::runtime_error("Config JSON doesn't exist!\n");
   std::ifstream i(config_filename);
@@ -57,86 +54,69 @@ std::unique_ptr<DSP> get_dsp(const std::filesystem::path config_filename)
   nlohmann::json config = j["config"];
   std::vector<float> params = _get_weights(j, config_filename);
 
-  if (architecture == "Linear")
-  {
+  if (architecture == "Linear") {
     const int receptive_field = config["receptive_field"];
     const bool _bias = config["bias"];
     return std::make_unique<Linear>(receptive_field, _bias, params);
-  }
-  else if (architecture == "ConvNet")
-  {
+  } else if (architecture == "ConvNet") {
     const int channels = config["channels"];
     const bool batchnorm = config["batchnorm"];
     std::vector<int> dilations;
     for (int i = 0; i < config["dilations"].size(); i++)
       dilations.push_back(config["dilations"][i]);
     const std::string activation = config["activation"];
-    return std::make_unique<convnet::ConvNet>(channels, dilations, batchnorm, activation, params);
-  }
-  else if (architecture == "LSTM")
-  {
+    return std::make_unique<convnet::ConvNet>(channels, dilations, batchnorm,
+                                              activation, params);
+  } else if (architecture == "LSTM") {
     const int num_layers = config["num_layers"];
     const int input_size = config["input_size"];
     const int hidden_size = config["hidden_size"];
-    auto json = nlohmann::json {};
-    return std::make_unique<lstm::LSTM>(num_layers, input_size, hidden_size, params, json);
-  }
-  else if (architecture == "CatLSTM")
-  {
+    auto json = nlohmann::json{};
+    return std::make_unique<lstm::LSTM>(num_layers, input_size, hidden_size,
+                                        params, json);
+  } else if (architecture == "CatLSTM") {
     const int num_layers = config["num_layers"];
     const int input_size = config["input_size"];
     const int hidden_size = config["hidden_size"];
-    return std::make_unique<lstm::LSTM>(num_layers, input_size, hidden_size, params, config["parametric"]);
-  }
-  else if (architecture == "WaveNet" || architecture == "CatWaveNet")
-  {
+    return std::make_unique<lstm::LSTM>(num_layers, input_size, hidden_size,
+                                        params, config["parametric"]);
+  } else if (architecture == "WaveNet" || architecture == "CatWaveNet") {
     std::vector<wavenet::LayerArrayParams> layer_array_params;
     for (int i = 0; i < config["layers"].size(); i++) {
       nlohmann::json layer_config = config["layers"][i];
       std::vector<int> dilations;
       for (int j = 0; j < layer_config["dilations"].size(); j++)
         dilations.push_back(layer_config["dilations"][j]);
-      layer_array_params.push_back(
-        wavenet::LayerArrayParams(
-          layer_config["input_size"],
-          layer_config["condition_size"],
-          layer_config["head_size"],
-          layer_config["channels"],
-          layer_config["kernel_size"],
-          dilations,
-          layer_config["activation"],
-          layer_config["gated"],
-          layer_config["head_bias"]
-        )
-      );
+      layer_array_params.push_back(wavenet::LayerArrayParams(
+          layer_config["input_size"], layer_config["condition_size"],
+          layer_config["head_size"], layer_config["channels"],
+          layer_config["kernel_size"], dilations, layer_config["activation"],
+          layer_config["gated"], layer_config["head_bias"]));
     }
     const bool with_head = config["head"] == NULL;
     const float head_scale = config["head_scale"];
-    // Solves compilation issue on macOS Error: No matching constructor for initialization of 'wavenet::WaveNet'
-    // Solution from https://stackoverflow.com/a/73956681/3768284
-    auto parametric_json = architecture == "CatWaveNet" ? config["parametric"] : nlohmann::json{};
+    // Solves compilation issue on macOS Error: No matching constructor for
+    // initialization of 'wavenet::WaveNet' Solution from
+    // https://stackoverflow.com/a/73956681/3768284
+    auto parametric_json =
+        architecture == "CatWaveNet" ? config["parametric"] : nlohmann::json{};
     return std::make_unique<wavenet::WaveNet>(
-      layer_array_params,
-      head_scale,
-      with_head,
-      parametric_json,
-      params
-    );
-  }
-  else
-  {
+        layer_array_params, head_scale, with_head, parametric_json, params);
+  } else {
     throw std::runtime_error("Unrecognized architecture");
   }
 }
 
-std::unique_ptr<DSP> get_hard_dsp()
-{
+std::unique_ptr<DSP> get_hard_dsp() {
   // Values are defined in HardCodedModel.h
   verify_config_version(std::string(PYTHON_MODEL_VERSION));
 
   // Uncomment the line that corresponds to the model type that you're using.
-  
-  //return std::make_unique<convnet::ConvNet>(CHANNELS, DILATIONS, BATCHNORM, ACTIVATION, PARAMS);
-  //return std::make_unique<wavenet::WaveNet>(LAYER_ARRAY_PARAMS, HEAD_SCALE, WITH_HEAD, PARAMETRIC, PARAMS);
-  return std::make_unique<lstm::LSTM>(NUM_LAYERS, INPUT_SIZE, HIDDEN_SIZE, PARAMS, PARAMETRIC);
+
+  // return std::make_unique<convnet::ConvNet>(CHANNELS, DILATIONS, BATCHNORM,
+  // ACTIVATION, PARAMS); return
+  // std::make_unique<wavenet::WaveNet>(LAYER_ARRAY_PARAMS, HEAD_SCALE,
+  // WITH_HEAD, PARAMETRIC, PARAMS);
+  return std::make_unique<lstm::LSTM>(NUM_LAYERS, INPUT_SIZE, HIDDEN_SIZE,
+                                      PARAMS, PARAMETRIC);
 }
