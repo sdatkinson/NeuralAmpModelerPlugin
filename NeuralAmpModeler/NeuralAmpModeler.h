cc640a219f6385652a358a29ec0970718e0b8e87
Steven Atkinson <steven@atkinson.mn>
2023-02-05 14:25:33 -0800

    Get rid of choc and comment out unused variables in cnpy

--- NeuralAmpModeler/NeuralAmpModeler.h
+++ NeuralAmpModeler/NeuralAmpModeler.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "choc_DisableAllWarnings.h"
-#include "choc_ReenableAllWarnings.h"
 #include "dsp.h"
 #include "dsp/ImpulseResponse.h"
 #include "dsp/RecursiveLinearFilter.h"
