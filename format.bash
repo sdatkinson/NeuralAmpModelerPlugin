19f1b604c8fb76eb59d331ca9baeb18278e40d77
Steven Atkinson <steven@atkinson.mn>
2023-02-04 19:46:09 -0800

    Additions to formatter script, got the template project too

--- format.bash
+++ format.bash
@@ -4,4 +4,12 @@
 # Usage:
 # $ bash format.bash
 
+echo "Formatting..."
+
 git ls-files "*.h" "*.cpp" | xargs clang-format --style=llvm -i
+
+echo "Formatting complete!"
+echo "You can stage all of the files using:"
+echo ""
+echo '  git ls-files "*.h" "*.cpp" | xargs git add'
+echo ""
