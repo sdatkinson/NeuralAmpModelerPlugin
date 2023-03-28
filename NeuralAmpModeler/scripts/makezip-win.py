5ce943d0d527e2e974640345cff4e7734475c219
slowy07 <slowy.arfy@gmail.com>
2023-03-28 11:15:25 +0700

    chore: adding pre commit configuration
    fix: fixing style code following PEP8 python code
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- NeuralAmpModeler/scripts/makezip-win.py
+++ NeuralAmpModeler/scripts/makezip-win.py
@@ -5,69 +5,76 @@ projectpath = os.path.abspath(os.path.join(scriptpath, os.pardir))
 
 IPLUG2_ROOT = "..\..\iPlug2"
 
-sys.path.insert(0, os.path.join(scriptpath, IPLUG2_ROOT + '\Scripts'))
+sys.path.insert(0, os.path.join(scriptpath, IPLUG2_ROOT + "\Scripts"))
 
 from get_archive_name import get_archive_name
 
+
 def main():
-  if len(sys.argv) != 3:
-    print("Usage: make_zip.py demo[0/1] zip[0/1]")
-    sys.exit(1)
-  else:
-    demo=int(sys.argv[1])
-    zip=int(sys.argv[2])
+    if len(sys.argv) != 3:
+        print("Usage: make_zip.py demo[0/1] zip[0/1]")
+        sys.exit(1)
+    else:
+        demo = int(sys.argv[1])
+        zip = int(sys.argv[2])
 
-  dir = projectpath + "\\build-win\\out"
-  
-  if os.path.exists(dir):
-    shutil.rmtree(dir)
-  
-  os.makedirs(dir)
-  
-  files = []
+    dir = projectpath + "\\build-win\\out"
 
-  if not zip:
-    installer = "\\build-win\\installer\\NeuralAmpModeler Installer.exe"
-    
-    if demo:
-      installer = "\\build-win\\installer\\NeuralAmpModeler Demo Installer.exe"
-    
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+
+    os.makedirs(dir)
+
+    files = []
+
+    if not zip:
+        installer = "\\build-win\\installer\\NeuralAmpModeler Installer.exe"
+
+        if demo:
+            installer = "\\build-win\\installer\\NeuralAmpModeler Demo Installer.exe"
+
+        files = [
+            projectpath + installer,
+            projectpath + "\\installer\\changelog.txt",
+            projectpath + "\\installer\\known-issues.txt",
+            projectpath + "\\manual\\NeuralAmpModeler manual.pdf",
+        ]
+    else:
+        files = [
+            projectpath
+            + "\\build-win\\NeuralAmpModeler.vst3\\Contents\\x86_64-win\\NeuralAmpModeler.vst3",
+            projectpath + "\\build-win\\NeuralAmpModeler_x64.exe",
+        ]
+
+    zipname = get_archive_name(projectpath, "win", "demo" if demo == 1 else "full")
+
+    zf = zipfile.ZipFile(
+        projectpath + "\\build-win\\out\\" + zipname + ".zip", mode="w"
+    )
+
+    for f in files:
+        print("adding " + f)
+        zf.write(f, os.path.basename(f), zipfile.ZIP_DEFLATED)
+
+    zf.close()
+    print("wrote " + zipname)
+
+    zf = zipfile.ZipFile(
+        projectpath + "\\build-win\\out\\" + zipname + "-pdbs.zip", mode="w"
+    )
+
     files = [
-      projectpath + installer,
-      projectpath + "\\installer\\changelog.txt",
-      projectpath + "\\installer\\known-issues.txt",
-      projectpath + "\\manual\\NeuralAmpModeler manual.pdf" 
-    ]
-  else:
-    files = [
-      projectpath + "\\build-win\\NeuralAmpModeler.vst3\\Contents\\x86_64-win\\NeuralAmpModeler.vst3",
-      projectpath + "\\build-win\\NeuralAmpModeler_x64.exe"  
+        projectpath + "\\build-win\\pdbs\\NeuralAmpModeler-vst3_x64.pdb",
+        projectpath + "\\build-win\\pdbs\\NeuralAmpModeler-app_x64.pdb",
     ]
 
-  zipname = get_archive_name(projectpath, "win", "demo" if demo == 1 else "full" )
+    for f in files:
+        print("adding " + f)
+        zf.write(f, os.path.basename(f), zipfile.ZIP_DEFLATED)
 
-  zf = zipfile.ZipFile(projectpath + "\\build-win\\out\\" + zipname + ".zip", mode="w")
+    zf.close()
+    print("wrote " + zipname)
 
-  for f in files:
-    print("adding " + f)
-    zf.write(f, os.path.basename(f), zipfile.ZIP_DEFLATED)
 
-  zf.close()
-  print("wrote " + zipname)
-
-  zf = zipfile.ZipFile(projectpath + "\\build-win\\out\\" + zipname + "-pdbs.zip", mode="w")
-
-  files = [
-    projectpath + "\\build-win\\pdbs\\NeuralAmpModeler-vst3_x64.pdb",
-    projectpath + "\\build-win\\pdbs\\NeuralAmpModeler-app_x64.pdb"  
-  ]
-
-  for f in files:
-    print("adding " + f)
-    zf.write(f, os.path.basename(f), zipfile.ZIP_DEFLATED)
-
-  zf.close()
-  print("wrote " + zipname)
-
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
