f2d1a75b080cf81d3a73cc3568e742139b835a52
Steven Atkinson <steven@atkinson.mn>
2023-04-09 12:02:41 -0700

    Update code style (#180)
    * Formatting style
    
    * Apply formatting

--- TemplateProject/TemplateProject.h
+++ TemplateProject/TemplateProject.h
@@ -4,26 +4,33 @@
 
 const int kNumPresets = 1;
 
-enum EParams { kParamGain = 0, kNumParams };
+enum EParams
+{
+  kParamGain = 0,
+  kNumParams
+};
 
-enum ECtrlTags { kCtrlTagVersionNumber = 0, kCtrlTagSlider, kCtrlTagTitle };
+enum ECtrlTags
+{
+  kCtrlTagVersionNumber = 0,
+  kCtrlTagSlider,
+  kCtrlTagTitle
+};
 
 using namespace iplug;
 using namespace igraphics;
 
-class TemplateProject final : public Plugin {
+class TemplateProject final : public Plugin
+{
 public:
-  TemplateProject(const InstanceInfo &info);
+  TemplateProject(const InstanceInfo& info);
 
 #if IPLUG_EDITOR
   void OnParentWindowResize(int width, int height) override;
-  bool OnHostRequestingSupportedViewConfiguration(int width,
-                                                  int height) override {
-    return true;
-  }
+  bool OnHostRequestingSupportedViewConfiguration(int width, int height) override { return true; }
 #endif
 
 #if IPLUG_DSP // http://bit.ly/2S64BDd
-  void ProcessBlock(sample **inputs, sample **outputs, int nFrames) override;
+  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
 #endif
 };
