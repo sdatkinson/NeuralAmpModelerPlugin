d0d5a389413eb1be0229e5181bef7bac211e0aaf
Steven Atkinson <steven@atkinson.mn>
2023-04-30 20:29:41 -0700

    Revert to standard color

--- NeuralAmpModeler/Colors.h
+++ NeuralAmpModeler/Colors.h
@@ -69,8 +69,8 @@ const iplug::igraphics::IColor NAM_3(255, 162, 178, 191); // Cadet Blue Crayola
 
 // Evan Heritage theme colors
 const iplug::igraphics::IColor NAM_0(0, 18, 17, 19); // Transparent
-// const iplug::igraphics::IColor NAM_THEMECOLOR(255, 80, 133, 232); // Azure
-const iplug::igraphics::IColor NAM_THEMECOLOR(255, 23, 190, 187); // Custom :)
+const iplug::igraphics::IColor NAM_THEMECOLOR(255, 80, 133, 232); // Azure
+// const iplug::igraphics::IColor NAM_THEMECOLOR(255, 23, 190, 187); // Custom :)
 const iplug::igraphics::IColor NAM_THEMEFONTCOLOR(255, 242, 242, 242); // Dark White
 
 // Misc
