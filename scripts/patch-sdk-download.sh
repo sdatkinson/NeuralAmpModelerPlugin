87c6db6b2f8d57666fc003e1c2e535b2edf7c808
Steven Atkinson <steven@atkinson.mn>
2023-05-07 19:37:21 -0700

    MacOS flag

--- scripts/patch-sdk-download.sh
+++ scripts/patch-sdk-download.sh
@@ -9,4 +9,12 @@ IPLUG_DIR=../iPlug2/Dependencies/iPlug
 
 cp resources/${PATCH_SCRIPT_NAME} ${IPLUG_DIR}
 
-sed -i "s/download-vst3-sdk.sh/${PATCH_SCRIPT_NAME}/g" ${IPLUG_DIR}/download-iplug-sdks.sh
+if [ $(uname -s) == "Darwin" ]; then
+    echo "MacOS detected"
+    IN_PLACE_FLAG=-i.bak
+else
+    echo "Linux detected"
+    IN_PLACE_FLAG=-i
+fi
+
+sed ${IN_PLACE_FLAG} "s/download-vst3-sdk.sh/${PATCH_SCRIPT_NAME}/g" ${IPLUG_DIR}/download-iplug-sdks.sh
